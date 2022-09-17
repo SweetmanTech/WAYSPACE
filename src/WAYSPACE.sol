f54ba742082ed7874083f58f4487a0f866b7d3ae
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-16 22:25:58 -0300

    I init foundry.

---
+++ src/WAYSPACE.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "./lib/PuzzleDrop.sol";
+import "./lib/AlbumMetadata.sol";
+
+contract WAYSPACE is AlbumMetadata, PuzzleDrop {
+    constructor(string[] memory _musicMetadata)
+        PuzzleDrop("WAYSPACE", "JACKIE")
+    {
+        setupAlbumMetadata(_musicMetadata);
+    }
+
+    /// @notice This allows the user to purchase a edition edition
+    /// at the given price in the contract.
+    function purchase(uint256 _quantity)
+        external
+        payable
+        onlyPublicSaleActive
+        onlyValidPrice(singlePrice, _quantity)
+        returns (uint256)
+    {
+        uint256 firstMintedTokenId = _purchase(_quantity, 1);
+        return firstMintedTokenId;
+    }
+
+    /// @notice This allows the user to purchase a edition edition
+    /// at the given price in the contract.
+    function purchaseBundle(uint256 _quantity)
+        external
+        payable
+        onlyPublicSaleActive
+        onlyValidPrice(bundlePrice, _quantity)
+        returns (uint256)
+    {
+        uint8 songIdOne = 1;
+        uint8 songIdTwo = 2;
+        _purchase(_quantity, songIdOne);
+        uint256 firstMintedTokenId = _purchase(_quantity, songIdTwo);
+        return firstMintedTokenId;
+    }
+
+    /// @notice This allows the user to purchase a edition edition
+    /// at the given price in the contract.
+    function _purchase(uint256 quantity, uint8 _songId)
+        internal
+        onlyValidSongId(_songId)
+        returns (uint256)
+    {
+        uint256 start = _nextTokenId();
+        _mint(msg.sender, quantity);
+        _setSongURI(start, quantity, _songId);
+
+        emit Sale({
+            to: msg.sender,
+            quantity: quantity,
+            pricePerToken: bundlePrice,
+            firstPurchasedTokenId: start
+        });
+        return start;
+    }
+
+    /// @notice Returns the Uniform Resource Identifier (URI) for `tokenId` token.
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        uint8 songId = songIds[tokenId];
+        return songURI(songId);
+    }
+}
---
+++ test/lib/PuzzleDrop.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/lib/PuzzleDrop.sol";
+
+contract PuzzleDropTest is Test {
+    function setUp() public {}
+
+    PuzzleDrop pd;
+
+    function testCan_publicSaleStart() public {
+        pd = new PuzzleDrop("WAYSPACE", "JACKIE");
+        uint64 start = pd.publicSaleStart();
+        uint64 nowSec = uint64(block.timestamp);
+        assertEq(start, nowSec);
+    }
+
+    function testCan_publicSaleEnd() public {
+        pd = new PuzzleDrop("WAYSPACE", "JACKIE");
+        uint64 end = pd.publicSaleEnd();
+        uint64 halloween = 1667260799;
+        assertEq(end, halloween);
+    }
+
+    function testCan_dropsCreated() public {
+        pd = new PuzzleDrop("WAYSPACE", "JACKIE");
+        assertEq(pd.dropsCreated(), 2);
+
+        vm.warp(block.timestamp + pd.secondsBetweenDrops());
+        assertEq(pd.dropsCreated(), 4);
+
+        vm.warp(block.timestamp + pd.secondsBetweenDrops());
+        assertEq(pd.dropsCreated(), 6);
+
+        vm.warp(block.timestamp + pd.secondsBetweenDrops());
+        assertEq(pd.dropsCreated(), 8);
+
+        vm.warp(block.timestamp + pd.secondsBetweenDrops());
+        assertEq(pd.dropsCreated(), 10);
+
+        vm.warp(block.timestamp + pd.secondsBetweenDrops());
+        assertEq(pd.dropsCreated(), 12);
+
+        vm.warp(block.timestamp + pd.secondsBetweenDrops());
+        assertEq(pd.dropsCreated(), 12);
+    }
+}
