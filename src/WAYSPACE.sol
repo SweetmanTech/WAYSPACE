40f9f624eabb59a53e8298daf0a8b7cb4b5093b3
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-16 23:39:38 -0300

    I update purchaseBundle to always bundle the last 2 tokens.

--- src/WAYSPACE.sol
+++ src/WAYSPACE.sol
@@ -33,10 +33,11 @@ contract WAYSPACE is AlbumMetadata, PuzzleDrop {
         onlyValidPrice(bundlePrice, _quantity)
         returns (uint256)
     {
-        uint8 songIdOne = 1;
-        uint8 songIdTwo = 2;
-        _purchase(_quantity, songIdOne);
-        return _purchase(_quantity, songIdTwo);
+        uint8 songIdTwo = dropsCreated();
+        uint8 songIdOne = songIdTwo - 1;
+        uint256 firstMintedTokenId = _purchase(_quantity, songIdOne);
+        _purchase(_quantity, songIdTwo);
+        return firstMintedTokenId;
     }
 
     /// @notice This allows the user to purchase a edition edition
