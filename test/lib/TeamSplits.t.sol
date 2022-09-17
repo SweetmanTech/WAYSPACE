d05fca58f78d497c454511305556ae8f7c7caa67
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-17 18:14:19 -0300

    I add test to verify ability to payout all splits.

--- test/lib/TeamSplits.t.sol
+++ test/lib/TeamSplits.t.sol
@@ -1,17 +1,30 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
 import "forge-std/Test.sol";
 import "src/lib/TeamSplits.sol";
 
-contract TeamSplitsTest is Test {
-    TeamSplits ts;
+contract TeamSplitsTest is Test, TeamSplits {
+    function setUp() public {}
 
-    function setUp() public {
-        ts = new TeamSplits();
+    function testCan_getListOfRecipients() public {
+        assertEq(recipients().length, 14);
     }
 
-    function testCan_publicSaleStart() public {
-        assertEq(ts.recipients().length, 14);
+    function testFail_payout14thSplit() public {
+        /// @dev there are 14 recipients.
+        _paySplit(14, 100);
+    }
+
+    function testCan_payoutSplit() public {
+        for (uint8 i = 0; i < recipients().length; i++) {
+            assertEq(address(recipients()[i]).balance, 0);
+        }
+
+        for (uint8 i = 0; i < recipients().length; i++) {
+            uint256 preBalance = address(recipients()[i]).balance;
+            _paySplit(i, 100);
+            assertEq(address(recipients()[i]).balance, preBalance + 100);
+        }
     }
 }
